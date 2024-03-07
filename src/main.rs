87a21d93cdda74f342eef29444e76e58fce1cb88
Carson <carsonburke22@gmail.com>
2024-03-07 15:25:59 -0800

    basic application UI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use iced::{self, executor, widget::{column, container, text}, Application, Command, Element, Settings, Theme};
+use iced::{
+    executor, theme,
+    widget::{column, container, horizontal_space, row, scrollable, text, Space},
+    Alignment, Application, Command, Element, Font, Length, Settings, Theme,
+};
 pub fn main() -> iced::Result {
     App::run(Settings::default())
 }
 
 #[derive(Default)]
-struct App {
-    
-}
+struct App {}
 
 #[derive(Debug, Clone)]
 enum Message {
@@ -20,12 +22,7 @@ impl Application for App {
     type Flags = ();
 
     fn new(_flags: ()) -> (Self, Command<Message>) {
-        (
-            Self {
-                ..Self::default()
-            },
-            Command::none(),
-        )
+        (Self { ..Self::default() }, Command::none())
     }
 
     fn title(&self) -> String {
@@ -33,23 +30,82 @@ impl Application for App {
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
-        
         match message {
             Message::Tick => {
                 println!("ticked, {:?}", message);
             }
         }
-        
+
         Command::none()
     }
 
     fn view(&self) -> Element<Message> {
+        let sidebar_header = row![
+            text(String::from("Prefs")),
+            // horizontal_space(),
+            text(String::from("C Tasks")),
+            // horizontal_space(),
+            text(String::from("toggle"))
+        ].spacing(10);
 
-        let content = column![
-            text(String::from("Hello World")),
+        let sidebar_content = column![
+            text(String::from("applications")),
+            text(String::from("processes")),
+            text(String::from("cpu")),
+            text(String::from("memory")),
+            text(String::from("gpu 1")),
+            text(String::from("gpu 2")),
+            text(String::from("disk")),
+            text(String::from("wifi")),
+            text(String::from("Ethernet if exists")),
         ];
 
-        container(content).into()
+        let sidebar = container(column![
+            sidebar_header,
+            sidebar_content,
+        ].spacing(20))
+        .style(theme::Container::Box)
+        .height(Length::Fill)
+        .padding(20)
+        .width(Length::Shrink);
+
+        let header = container(
+            row![
+                horizontal_space(),
+                text(String::from("Switcher"))
+                    .size(20)
+                    .font(Font::MONOSPACE),
+                horizontal_space(),
+                text(String::from("- x")),
+            ],
+        )
+        .width(Length::Fill)
+        .style(|theme: &Theme| {
+            let palette = theme.extended_palette();
+
+            container::Appearance::default().with_border(palette.background.strong.color, 1)
+        })
+        .padding(20);
+
+        let footer = container(row![text(String::from("Footer Text"))])
+            .style(theme::Container::Box)
+            .center_y()
+            .center_x()
+            .width(Length::Fill)
+            .padding(20);
+
+        let main = container(scrollable(
+            column![text("hello".to_string())].spacing(10),
+        ))
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .padding(20);
+    
+        let left = sidebar;
+        let right = column![header, main, footer]
+        .width(Length::FillPortion(3));
+
+        row![left, right].into()
     }
 
     fn theme(&self) -> Theme {
