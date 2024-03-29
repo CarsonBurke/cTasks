4ca16e8c107862bdaf6d100b00f31c5bff6a1d7d
Carson <carsonburke22@gmail.com>
2024-03-29 14:33:44 -0700

    remove dead general widget code

--- src/general_widgets/section_box.rs
+++ src/general_widgets/section_box.rs
@@ -12,14 +12,7 @@ pub fn section_box<'a, Message: 'a>(
     header: (Text<'a>, Text<'a>),
     body: Column<'a, Message>,
 ) -> Column<'a, Message> {
-    // let header = row![
-    //     text(header.0)
-    //         .font(iced_aw::BOOTSTRAP_FONT)
-    //         .size(font_sizes::H2),
-    //     text(String::from(header.1)).size(font_sizes::H2),
-    // ].spacing(padding::MAIN);
 
-    // let header = header.spacing(padding::MAIN);
     let header =
         row![header.0.size(font_sizes::H2), header.1.size(font_sizes::H2)].spacing(padding::MAIN);
 
